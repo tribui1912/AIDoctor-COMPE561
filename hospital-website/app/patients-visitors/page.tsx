6d232b46d938f26d5e61df3700da699e9ced0b0a
Tri Bui <tribui1912@gmail.com>
2024-11-16 19:32:28 -0800

    Reapply "Sticky h1 and paragraph spacing to test."
    This reverts commit 1b472801422e6b1fec07b80d4810e8c7036b59e8.

--- hospital-website/app/patients-visitors/page.tsx
+++ hospital-website/app/patients-visitors/page.tsx
@@ -2,8 +2,8 @@ export default function PatientsVisitors() {
     return (
       <div>
         <h1 className="text-3xl font-bold mb-4">Patients & Visitors</h1>
-        <p>Information for patients and visitors to make your hospital experience as comfortable as possible.</p>
-        {/* Add more content here */}
+        <p style={{height:'2000px'}}> Information for patients and visitors to make your hospital experience as comfortable as possible.
+	</p>
       </div>
     )
   }
