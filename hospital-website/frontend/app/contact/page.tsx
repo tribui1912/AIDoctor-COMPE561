a665501f9c4d08aa35b34172910f37a5384ab78a
Reem Awad <reemawad@Reems-MacBook-Pro.local>
2024-11-26 12:52:37 -0800

    3 Locations on Location Page

--- hospital-website/frontend/app/contact/page.tsx
+++ hospital-website/frontend/app/contact/page.tsx
@@ -66,10 +66,10 @@ export default function Contact() {
       </div>
 
       <section className="mt-8">
-        <h2 className="text-2xl font-semibold mb-4">Find Us</h2>
+        <h2 className="text-2xl font-semibold mb-4">Find Us </h2>
         <div className="aspect-w-16 aspect-h-9">
           <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.2176767606297!2d-73.98823492426385!3d40.748440537541225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1682805705560!5m2!1sen!2sus" 
+            src="https://www.google.com/maps/embed/v1/place?q=San+diego+state+university&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" 
             width="600" 
             height="450" 
             style={{border:0}} 
